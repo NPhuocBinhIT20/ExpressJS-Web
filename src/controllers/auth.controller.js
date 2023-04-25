97aefe87650ac92da29737163ccc628eca7b4155
unknown <binhnguyen110120@gmail.com>
2023-04-25 18:01:15 +0700

    24/04/2023

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const authService = require("../services/auth.service");
 const bcrypt = require("bcryptjs");
+const Promise = require("bluebird");
 module.exports = {
   addUser: (req, res) => {
     const data = req.body;
@@ -7,11 +8,11 @@ module.exports = {
     authService
       .addUser(data.username, data.email, data.password)
       .then((status) => {
-        if (status) res.status(200).json({ message: "Success" });
-        else res.status(400).json({ message: "Username exist" });
+        if (status) res.status(200).json({message: "Success"});
+        else res.status(400).json({message: "Username exist"});
       })
       .catch((err) => {
-        res.status(500).json({ error: err });
+        res.status(500).json({error: err});
       });
   },
 
@@ -30,48 +31,67 @@ module.exports = {
               email: data.email,
             });
           } else {
-            res.status(400).json({ message: "User does not exist" });
+            res.status(400).json({message: "User does not exist"});
           }
         } else {
-          res.status(400).json({ message: "User does not exist" });
+          res.status(400).json({message: "User does not exist"});
         }
       })
       .catch((err) => {
-        res.status(500).json({ error: err });
+        res.status(500).json({error: err});
       });
   },
   getAll: (_, res) => {
     authService
       .getAll()
       .then((data) => {
-        res.status(200).json({ users: data });
+        res.status(200).json({users: data});
       })
       .catch((err) => {
-        res.status(500).json({ error: err });
+        res.status(500).json({error: err});
       });
   },
-   //get item by id
-   getUserById: (req, res) => {
+  getUserById: (req, res) => {
     const id = req.params.id;
     authService
       .getUserById(id)
       .then((data) => {
-        res.status(200).json({ user: data });
+        res.status(200).json({user: data});
       })
       .catch((err) => {
-        res.status(500).json({ error: err });
+        res.status(500).json({error: err});
       });
   },
-   //delete product by id
-   updateUserById: (req, res) => {
+  updateUserById: (req, res) => {
     const id = req.params.id;
+    const data = req.body;
+
     authService
-      .updateUserById(id)
-      .then((data) => {
-        res.status(200).json({ user: data });
+      .updateUserById(data.username, data.email, data.password)
+      .then((status) => {
+        if (status) res.status(200).json({message: "Update Success"});
+        else res.status(400).json({message: "Error update"});
       })
       .catch((err) => {
-        res.status(500).json({ error: err });
+        res.status(500).json({error: err});
       });
+  },
+  login: (username, password) => {
+    return Promise((resolve, reject) => {
+      if (true) {
+        resolve({
+          success: true,
+          data: {
+            _id: 1,
+            token: "1234",
+          },
+        });
+      } else {
+        reject({
+          success: false,
+          message: "Email not exist",
+        });
+      }
+    });
   },
 };
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,4 +4,5 @@ module.exports = function (app) {
   app.route("/index").get(auth.getAll); 
   app.route("/register").post(auth.addUser);
   app.route("/login").post(auth.getUser);
+  app.route("/user/:id").get(auth.getUserById).put(auth.updateUserById);
 };
